80e19aa26934822f4b3928872b7c2265b822eb03
pan yong <will.mpan@gmail.com>
2019-11-11 10:00:46 +0800

    update

---
+++ config/in.js
@@ -0,0 +1,20 @@
+window.language = {
+	cosmetology: '医疗美容',
+	regenerate: '再生医疗',
+	anti_aging_care_beauty: '抗衰老护理、美颜',
+	beauty_medical_equipment: "美容医疗器械",
+	injection_therapy: "注射治疗",
+	taipu_services_regenerate: "泰璞健康提供的再生医疗服务：",
+	taipu_services_examination: "泰璞健康提供的赴日体检服务：",
+
+	server_line: "服务专线",
+	tel: "400-686-0831",
+	taipu_china: "泰璞中国",
+	taipu_jp: "泰璞日本",
+	address: "地址",
+	address_china: "上海市普陀区大渡河路388弄5号华宏商务中心14F",
+	address_jp: "〒104-0061 東京都中央区銀座6-10-1 GINZA SIX 8F",
+
+	company: "泰璞（上海）电子商务股份有限公司",
+	copy: "备案号：沪IVCP备1689067897668",
+}
